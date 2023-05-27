27e4c45ee78d5946d6c10852981bdfa750225ca5
Keisuke Tottori <totto8492@gmail.com>
2023-05-27 16:42:03 +0900

    cosmetics

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use embassy_executor::{Executor, Spawner};
+use embassy_executor::{Executor};
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::multicore::spawn_core1;
 #[allow(unused)]
 use rtt_target::rprintln as info;
 #[allow(unused)]
 use rtt_target::{rdbg, rprint, rprintln, rtt_init_print};
-use {{project-name}}::{core0, core1, CORE1_STACK, EXECUTOR0, EXECUTOR1};
+use {{project_name}}::{core0, core1, CORE1_STACK, EXECUTOR0, EXECUTOR1};
 use {panic_rtt_target as _, rtt_target as _};
 
 #[cortex_m_rt::entry]
