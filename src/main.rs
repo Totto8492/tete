4cffc59387cde58111b9042c78d3a48dbe9f93c8
Totto8492 <totto8492@gmail.com>
2023-06-05 19:13:52 +0900

    use pac instead of raw value

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ use {panic_rtt_target as _, rtt_target as _};
 mod tasks;
 use tasks::*;
 
-// https://github.com/rp-rs/rp-hal/blob/main/rp2040-hal-macros/src/lib.rs
 unsafe fn clear_locks() {
-    const SIO_BASE: u32 = 0xd0000000;
-    const SPINLOCK0_PTR: *mut u32 = (SIO_BASE + 0x100) as *mut u32;
+    // https://github.com/rp-rs/rp-hal/blob/main/rp2040-hal-macros/src/lib.rs
+    const SIO_BASE: *mut u8 = embassy_rp::pac::SIO.0;
+    const SPINLOCK0_PTR: *mut u32 = SIO_BASE.wrapping_add(0x100) as *mut u32;
     const SPINLOCK_COUNT: usize = 32;
     for i in 0..SPINLOCK_COUNT {
-        SPINLOCK0_PTR.wrapping_add(i).write_volatile(1);
+        SPINLOCK0_PTR.add(i).write_volatile(1);
     }
 }
 
