02de3f33c5a56a6850a2964226c6aed88a55919c
Totto8492 <totto8492@gmail.com>
2023-06-23 14:46:58 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use rtt_target as _;
 use rtt_target::rtt_init_print;
 use {{crate_name}}::run_task;
-use {panic_rtt_target as _, rtt_target as _};
 
 fn clear_locks() {
     // https://github.com/rp-rs/rp-hal/blob/main/rp2040-hal-macros/src/lib.rs
     for i in 0..32 {
         embassy_rp::pac::SIO.spinlock(i).write_value(1);
+    }
+}
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    cortex_m::interrupt::disable();
+    loop {
+        cortex_m::asm::udf();
     }
 }
 
@@ -32,7 +40,7 @@ mod sample {
 
     #[embassy_executor::task]
     pub(crate) async fn task(p: Peripherals) {
-        let mut led = Output::new(p.PIN_25, Level::High);
+        let mut led = Output::new(p.PIN_2, Level::High);
         loop {
             Timer::after(Duration::from_millis(200)).await;
             led.toggle();
