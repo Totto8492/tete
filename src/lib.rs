db9d5bf9b8a9a8d88b61f753e953931969b22d1d
Totto8492 <totto8492@gmail.com>
2023-06-23 14:52:34 +0900

    Totto8492 patch 1 (#1)
    * Update Cargo.toml
    
    * Update lib.rs
    
    * Update main.rs
    
    * Update main.rs
    
    * Update main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,53 +2,15 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use embassy_executor::Executor;
 use embassy_executor::_export::StaticCell;
-use embassy_executor::{Executor, InterruptExecutor, SendSpawner};
 pub use embassy_rp::interrupt::Priority;
-use embassy_rp::interrupt::{self, InterruptExt};
 use embassy_rp::multicore::{spawn_core1, Stack};
-use embassy_rp::pac::Interrupt;
 use embassy_rp::peripherals::CORE1;
 
 static mut CORE1_STACK: Stack<4096> = Stack::new();
 static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
 static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
-static EXECUTOR_IDLE: InterruptExecutor = InterruptExecutor::new();
-static EXECUTOR_LOW: InterruptExecutor = InterruptExecutor::new();
-static EXECUTOR_MEDIUM: InterruptExecutor = InterruptExecutor::new();
-static EXECUTOR_HIGH: InterruptExecutor = InterruptExecutor::new();
-
-#[embassy_rp::interrupt]
-unsafe fn SWI_IRQ_0() {
-    EXECUTOR_IDLE.on_interrupt()
-}
-
-#[embassy_rp::interrupt]
-unsafe fn SWI_IRQ_1() {
-    EXECUTOR_LOW.on_interrupt()
-}
-
-#[embassy_rp::interrupt]
-unsafe fn SWI_IRQ_2() {
-    EXECUTOR_MEDIUM.on_interrupt()
-}
-
-#[embassy_rp::interrupt]
-unsafe fn SWI_IRQ_3() {
-    EXECUTOR_HIGH.on_interrupt()
-}
-
-pub fn run_preemptive_task(prio: Priority, token: impl FnOnce(SendSpawner) + Send + 'static) {
-    let (irq, executor) = match prio {
-        Priority::P0 => (Interrupt::SWI_IRQ_0, &EXECUTOR_IDLE),
-        Priority::P1 => (Interrupt::SWI_IRQ_1, &EXECUTOR_LOW),
-        Priority::P2 => (Interrupt::SWI_IRQ_2, &EXECUTOR_MEDIUM),
-        Priority::P3 => (Interrupt::SWI_IRQ_3, &EXECUTOR_HIGH),
-    };
-    irq.set_priority(prio);
-    let spawner = executor.start(irq);
-    token(spawner);
-}
 
 pub fn run_task_on(core1: CORE1, token: impl FnOnce(embassy_executor::Spawner) + Send + 'static) {
     spawn_core1(core1, unsafe { &mut CORE1_STACK }, move || {
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use rtt_target::rtt_init_print;
 use {{crate_name}}::run_task;
-use {panic_rtt_target as _, rtt_target as _};
+use rtt_target::rtt_init_print;
+use rtt_target as _;
 
 fn clear_locks() {
     // https://github.com/rp-rs/rp-hal/blob/main/rp2040-hal-macros/src/lib.rs
     for i in 0..32 {
         embassy_rp::pac::SIO.spinlock(i).write_value(1);
+    }
+}
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    cortex_m::interrupt::disable();
+    loop {
+        cortex_m::asm::udf();
     }
 }
 
