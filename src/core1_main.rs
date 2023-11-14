0b41ace592aff0071c13193baf402921ee1e6ecd
Totto8492 <totto8492@gmail.com>
2023-11-14 19:34:49 +0900

    break th project (#17)

--- src/core1_main.rs
+++
@@ -1,8 +0,0 @@
-use embassy_time::{Instant, Timer};
-
-#[embassy_executor::task]
-pub async fn task() {
-    loop {
-        Timer::at(Instant::MAX).await;
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,27 @@
 #![warn(clippy::pedantic)]
 #![warn(clippy::nursery)]
 
+use embassy_executor::Executor;
 use embassy_rp::config::Config;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::multicore::{spawn_core1, Stack};
+use embassy_rp::peripherals::PIN_25;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::channel::Channel;
+use embassy_time::Timer;
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
+use static_cell::StaticCell;
 
-mod core0_main;
-mod core1_main;
-use {{crate_name}}::task::run;
+static mut CORE1_STACK: Stack<4096> = Stack::new();
+static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
+static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
+static CHANNEL: Channel<CriticalSectionRawMutex, LedState, 1> = Channel::new();
+
+enum LedState {
+    On,
+    Off,
+}
 
 pub fn clear_locks() {
     // https://github.com/rp-rs/rp-hal/blob/main/rp2040-hal-macros/src/lib.rs
@@ -27,9 +41,35 @@ fn main() -> ! {
     rtt_init_print!();
 
     let p = embassy_rp::init(Config::default());
-    rprintln!("init");
+    let led = Output::new(p.PIN_25, Level::Low);
 
-    run(|spawner| {
-        spawner.spawn(core0_main::task(spawner.make_send(), p)).unwrap();
+    spawn_core1(p.CORE1, unsafe { &mut CORE1_STACK }, move || {
+        let executor1 = EXECUTOR1.init(Executor::new());
+        executor1.run(|spawner| spawner.spawn(core1_task(led)).unwrap());
     });
+
+    let executor0 = EXECUTOR0.init(Executor::new());
+    executor0.run(|spawner| spawner.spawn(core0_task()).unwrap())
+}
+
+#[embassy_executor::task]
+async fn core0_task() {
+    rprintln!("Hello from core 0");
+    loop {
+        CHANNEL.send(LedState::On).await;
+        Timer::after_millis(100).await;
+        CHANNEL.send(LedState::Off).await;
+        Timer::after_millis(400).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn core1_task(mut led: Output<'static, PIN_25>) {
+    rprintln!("Hello from core 1");
+    loop {
+        match CHANNEL.receive().await {
+            LedState::On => led.set_high(),
+            LedState::Off => led.set_low(),
+        }
+    }
 }
