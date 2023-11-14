13b20c22a12430a2e5d394f4f0e31c65b0e757b3
Totto8492 <totto8492@gmail.com>
2023-11-14 18:53:22 +0900

    refactor 2 (#15)

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,14 @@
 #![feature(async_fn_in_trait)]
 #![allow(stable_features, unknown_lints, async_fn_in_trait)]
 #![warn(clippy::pedantic)]
-//#![warn(clippy::nursery)]
+#![warn(clippy::nursery)]
 
 use embassy_rp::config::Config;
-use embassy_rp::gpio::Pin;
-use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 mod core0_main;
 mod core1_main;
-use {{crate_name}}::task::{run, run_on};
-use {{crate_name}}::vreg::set_under_clock;
+use {{crate_name}}::task::run;
 
 pub fn clear_locks() {
     // https://github.com/rp-rs/rp-hal/blob/main/rp2040-hal-macros/src/lib.rs
@@ -26,15 +23,12 @@ pub fn clear_locks() {
 #[cortex_m_rt::entry]
 fn main() -> ! {
     clear_locks();
-    set_default_clock();
     rtt_init_print!();
 
     let p = embassy_rp::init(Config::default());
     rprintln!("init");
 
-    run_on(p.CORE1, |spawner| spawner.spawn(core1_main::task()).unwrap());
-
     run(|spawner| {
-        spawner.spawn(core0_main::task(p.PIN_25.degrade())).unwrap();
+        spawner.spawn(core0_main::task(spawner.make_send(), p)).unwrap();
     });
 }
