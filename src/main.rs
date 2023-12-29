d959b7a7c83dff68ee518b7c4dab03619a450dfd
Keisuke Tottori <totto8492@gmail.com>
2023-12-29 10:12:47 +0900

    use stable 1.75

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(type_alias_impl_trait)]
-#![feature(async_fn_in_trait)]
-#![allow(stable_features, unknown_lints, async_fn_in_trait)]
 #![warn(clippy::pedantic)]
 #![warn(clippy::nursery)]
 
